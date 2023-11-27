2153fda6d516b1ed063fede51f03d3064da9278a
AdilAlimgozha <alimgozha2003@gmail.com>
2023-11-27 18:14:01 +0600

    suna_erke_endterm

---
+++ end_suna_erke.py
@@ -0,0 +1,295 @@
+from tkinter import *
+from tkinter import ttk
+import numpy as np
+from mathematical_part_svd import *
+
+class Choose_size:
+    def create_w(self):
+        self.window = Tk()
+        self.window.resizable(False,False)
+        self.window.configure(bg='yellow')
+        self.window.geometry('200x200+600+300')
+
+        self.frame = Frame(self.window, bg='yellow')
+        self.frame.pack()
+        self.vlist = ["2","3","4","5"]
+        self.vlist2 = ["2","3","4","5"]
+        self.Combo1 = ttk.Combobox(self.frame, values = self.vlist, state='readonly')
+        self.Combo1.set("m")
+        self.Combo1.pack(padx = 5, pady = 5)
+        self.Combo2 = ttk.Combobox(self.frame, values = self.vlist2, state='readonly')
+        self.Combo2.set("n")
+        self.Combo2.pack(padx = 5, pady = 5)
+
+        self.L = Label(self.window, text = """Singular Value Decomposition
+Choose the size of a matrix""", bg='yellow', font = ("slant", 10))
+        self.L.place(x = 15, y = 100)
+
+        self.B = Button(self.window, text = 'Enter', font = "Scada", bg = "orange")
+        self.B.place(x = 75, y = 150)
+
+        self.button()
+
+        self.window.mainloop()
+    
+    def close_1_w_open_2_w(self, event):
+        self.get_n_m()
+        if self.size != ['m', 'n']:
+            self.window.destroy()
+            make_matrix.create_w()
+        else:
+            error_mn.create_w()
+            self.size = []
+
+    def button(self):
+        self.B.bind('<Button-1>', self.close_1_w_open_2_w)
+
+    def get_n_m(self):
+        self.size = []
+        self.size.append(self.Combo1.get())
+        self.size.append(self.Combo2.get())
+
+class Make_matrix:
+    def create_w(self):
+        self.window = Tk()
+        self.window.resizable(False,False)
+        self.window.configure(bg='cyan')
+        self.window.geometry('600x700+400+40')
+
+        self.L = Label(self.window, text = 'Construct your matrix', bg = "cyan", font = ("slant", 15))
+        self.L.place(x = 20, y = 20)
+
+        self.B = Button(self.window, text = 'Calculate SVD', font = ("slant", 15))
+        self.B.place(x = 250, y = 500)
+
+        self.rows = int(choose_size.size[0])
+        self.cols = int(choose_size.size[1])
+
+        self.entries_list = []
+        for i in range(self.rows):
+            ent_l = []
+            for j in range(self.cols):
+                ent_l.append(Entry())
+            self.entries_list.append(ent_l)
+        
+        xx = 250
+        yy = 50
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.entries_list[i][j].place(width = 30, height = 30, x = xx, y = yy)
+                xx += 50
+            xx = 250
+            yy += 50
+
+        self.button()
+            
+        self.window.mainloop()
+    
+    def get_entries(self, event):
+        self.A = []
+        for i in range(len(self.entries_list)):
+            rowA = []
+            for j in range (len(self.entries_list[i])):
+                if self.entries_list[i][j].get() == "":
+                    error_empty.create_w()
+                    rowA = []
+                    break
+                else:
+                    rowA.append(int(self.entries_list[i][j].get()))
+            if rowA != []:
+                self.A.append(rowA)
+        else:
+            self.npA = np.array(self.A)
+            self.window.destroy()
+            solution.create_w()
+
+    def button(self):
+        self.B.bind('<Button-1>', self.get_entries)
+
+class Solution:
+    def create_w(self):
+        self.window = Tk()
+        self.window.resizable(False,False)
+        self.window.configure(bg='greenyellow')
+        self.window.geometry('1500x765+200+40')
+        self.L = Label(self.window, text = ' Singular value decomposition: A = B*D*C', bg='greenyellow')
+        self.L.place(x = 200, y = 20)
+        self.B = Button(self.window, text = 'input another matrix', font = "Raleway", bg='lightskyblue')
+        self.B.place(x = 700, y = 500)
+
+
+
+
+
+
+
+
+        svd = SVD()
+        svd.svd(make_matrix.npA)
+
+        svd.adj()
+        svd.find_char_pol()
+        svd.coefficients()
+        svd.D()
+        svd.C()
+        svd.B()
+        svd.output()
+
+
+        self.L1 = Label(self.window, text = svd.D(), bg='greenyellow')
+        self.L1.place(x = 75, y = 50)
+        self.L11 = Label(self.window, text = "D =", bg='greenyellow')
+        self.L11.place(x = 50, y = 60)
+
+        self.L2 = Label(self.window, text = svd.C(), bg='greenyellow')
+        self.L2.place(x = 300, y = 50)
+        self.L22 = Label(self.window, text = "C =", bg='greenyellow')
+        self.L22.place(x = 275, y = 60)
+
+        self.L3 = Label(self.window, text = svd.B(), bg='greenyellow')
+        self.L3.place(x = 575, y = 50)
+        self.L33 = Label(self.window, text = "B =", bg='greenyellow')
+        self.L33.place(x = 550, y = 60)
+
+        self.L4 = Label(self.window, text = "Steps", font = ('Cambria', 20, 'bold'), bg='greenyellow')
+        self.L4.place(x = 280, y = 100)
+
+        self.L5 = Label(self.window, text = svd.adj(), bg='greenyellow')
+        self.L5.place(x = 60, y = 150)
+        self.L55 = Label(self.window, text = "1) AdjointA(transposed) * A:", bg='greenyellow')
+        self.L55.place(x = 60, y = 130)
+
+        self.L6 = Label(self.window, text = svd.find_char_pol(), bg='greenyellow')
+        self.L6.place(x = 60, y = 230)
+        self.L66 = Label(self.window, text = "2) Find characterictic polinomyal (A - xI):", bg='greenyellow')
+        self.L66.place(x = 60, y = 210)
+
+        self.L7 = Label(self.window, text = svd.coefficients(), bg='greenyellow')
+        self.L7.place(x = 60, y = 290)
+        self.L77 = Label(self.window, text = "3) Find eigenvalues:", bg='greenyellow')
+        self.L77.place(x = 60, y = 260)
+
+        self.L8 = Label(self.window, text = svd.D(), bg='greenyellow')
+        self.L8.place(x = 60, y = 350)
+        self.L88 = Label(self.window, text = "4) Find matrix D, where are square roots (r) of eigenvalues on main diagonal:", bg='greenyellow')
+        self.L88.place(x = 60, y = 320)
+
+        self.L9 = Label(self.window, text = svd.C(), bg='greenyellow')
+        self.L9.place(x = 60, y = 430)
+        self.L99 = Label(self.window, text = "5) Find orthogonal matrix C, which is transpose matrix of eigenvectors:", bg='greenyellow')
+        self.L99.place(x = 60, y = 400)
+
+        self.L10 = Label(self.window, text = svd.B(), bg='greenyellow')
+        self.L10.place(x = 60, y = 510)
+        self.L1010 = Label(self.window, text = "6) Find matrix B, where b = A*eigenvector/r:", bg='greenyellow')
+        self.L1010.place(x = 60, y = 480)
+
+        self.L11 = Label(self.window, text = make_matrix.npA, bg='greenyellow')
+        self.L11.place(x = 60, y = 600)
+        self.L1111 = Label(self.window, text = "7) Verify that decompisition is correct:", bg='greenyellow')
+        self.L1111.place(x = 60, y = 570)
+
+        self.L12 = Label(self.window, text = svd.B(), bg='greenyellow')
+        self.L12.place(x = 150, y = 600)
+        self.L1212 = Label(self.window, text = "=", bg='greenyellow')
+        self.L1212.place(x = 110, y = 615)
+
+        self.L13 = Label(self.window, text = svd.D(), bg='greenyellow')
+        self.L13.place(x = 460, y = 600)
+        self.L1313 = Label(self.window, text = "*", bg='greenyellow')
+        self.L1313.place(x = 445, y = 620)
+
+        self.L13 = Label(self.window, text = svd.C(), bg='greenyellow')
+        self.L13.place(x = 670, y = 600)
+        self.L1313 = Label(self.window, text = "*", bg='greenyellow')
+        self.L1313.place(x = 655, y = 620)
+        self.button()
+
+        self.window.mainloop()
+        
+
+    def open_1_w(self, event):
+        self.window.destroy()
+        choose_size.create_w()
+
+    def button(self):
+        self.B.bind('<Button-1>', self.open_1_w)
+
+class Error_mn:
+    def create_w(self):
+        self.window = Tk()
+        self.window.resizable(False,False)
+        self.window.configure(bg='red')
+        self.window.geometry('200x100+500+300')
+
+        self.L = Label(self.window, text = """ERROR!
+        Choose the size""", bg='red')
+        self.L.place(x = 50, y = 15)
+
+        self.B = Button(self.window, text = 'OK')
+        self.B.place(x = 100, y = 50)
+
+        self.button()
+
+        self.window.mainloop()
+
+    def close(self, event):
+        self.window.destroy()
+
+    def button(self):
+        self.B.bind('<Button-1>', self.close)
+
+        self.L = Label(self.window, text = """ERROR!
+The matrix is not in Z5""", bg='red')
+        self.L.place(x = 35, y = 15)
+
+        self.B = Button(self.window, text = 'OK')
+        self.B.place(x = 100, y = 50)
+
+        self.button()
+
+        self.window.mainloop()
+
+    def close(self, event):
+        self.window.destroy()
+
+    def button(self):
+        self.B.bind('<Button-1>', self.close)
+
+class Error_empty:
+    def create_w(self):
+        self.window = Tk()
+        self.window.resizable(False,False)
+        self.window.configure(bg='red')
+        self.window.geometry('200x100+500+300')
+
+        self.L = Label(self.window, text = """ERROR!
+The matrix has empty cells""", bg='red')
+        self.L.place(x = 35, y = 15)
+
+        self.B = Button(self.window, text = 'OK')
+        self.B.place(x = 100, y = 50)
+
+        self.button()
+
+        self.window.mainloop()
+
+    def close(self, event):
+        self.window.destroy()
+
+    def button(self):
+        self.B.bind('<Button-1>', self.close)
+
+    def close(self, event):
+        self.window.destroy()
+        make_matrix.create_w()
+
+    def button(self):
+        self.B.bind('<Button-1>', self.close)
+    
+choose_size = Choose_size()
+make_matrix = Make_matrix()
+solution = Solution()
+error_mn = Error_mn()
+error_empty = Error_empty()
+choose_size.create_w()
---
+++ mathematical_part_svd.py
@@ -0,0 +1,214 @@
+import numpy as np
+from math import sqrt
+from sympy import *
+from math import *
+import re
+
+
+class Common:
+
+    def Minor(self, A, i, j):
+        return [row[:j] + row[j+1:] for row in (A[:i]+A[i+1:])]
+
+    def determinant(self, A):
+        if len(A) == 1:
+            return A[0]
+        if len(A) == 2:
+            return A[0][0]*A[1][1]-A[0][1]*A[1][0]
+        if len(A) == 3:
+            return (A[0][0]*A[1][1]*A[2][2])+(A[0][1]*A[1][2]*A[2][0])+(A[0][2]*A[1][0]*A[2][1])-(A[0][2]*A[1][1]*A[2][0])-(A[0][0]*A[1][2]*A[2][1])-(A[0][1]*A[1][0]*A[2][2])
+        else:
+            self.determ = 0
+            for c in range(len(A)):
+                self.determ += ((-1)**c)*A[0][c] * self.determinant(self.Minor(A,0,c))
+            return self.determ
+    
+    def ref(self, A):
+        r, c = A.shape
+        if r == 0 or c == 0:
+            return A
+
+        for i in range(len(A)):
+            if A[i,0] != 0:
+                break
+        else:
+            B = self.ref(A[:,1:])
+            return np.hstack([A[:,:1], B])
+
+        if i > 0:
+            ith_row = A[i].copy()
+            A[i] = A[0]
+            A[0] = ith_row
+        A[0] = A[0] / A[0,0]
+        A[1:] -= A[0] * A[1:,0:1]
+        B = self.ref(A[1:,1:])
+        return np.vstack([A[:1], np.hstack([A[1:,:1], B]) ])
+    
+    def ctr_dig(self, n):
+        s = str(n)
+        x = re.findall('\.\d*', s)
+        return len(x[0]) - 1
+
+    def proj_v(self, v, w):
+        v = np.array(v)
+        w = np.array(w)
+        proj = ((np.dot(v, w))/(np.dot(w, w))) * w
+        return proj
+
+    def orthogonalization(self, s_v):
+        orthogonal_vectors = []
+        for i in range(len(s_v)):
+            if i == 0:
+                orthogonal_vectors.append(s_v[0])
+            elif i == 1:
+                w = s_v[i] - self.proj_v(s_v[i], orthogonal_vectors[i - 1])
+                orthogonal_vectors.append(w)
+            if i >= 2:
+                j = 2
+                w = s_v[i] - self.proj_v(s_v[i], orthogonal_vectors[i - 1])
+                while j != i+1:
+                    w = w - self.proj_v(s_v[i], orthogonal_vectors[i - j])
+                    j += 1
+                orthogonal_vectors.append(w)
+
+        summ_sq = []
+        for v in orthogonal_vectors:
+            summa = 0
+            for el in v:
+                summa += pow(el, 2)
+            summ_sq.append(sqrt(summa))
+        
+        for i in range(len(orthogonal_vectors)):
+            orthogonal_vectors[i] = (1/summ_sq[i]) * np.array(orthogonal_vectors[i])
+
+        return orthogonal_vectors
+
+
+
+class SVD:
+    def svd(self, A):
+        self.A = np.array(A)
+        return self.A
+
+    def adj(self):
+        self.I = np.identity(len(self.A))
+        self.A_t = np.transpose(self.A)
+        self.adjAA = np.dot(self.A_t, self.A)
+        self.AadjA = np.dot(self.A, self.A_t)
+        return self.adjAA
+
+    def find_char_pol(self):
+        M = Matrix(self.adjAA.tolist()) 
+        lamda = symbols('x') 
+        self.poly = str(M.charpoly(lamda))
+        return self.poly
+    
+    def coefficients(self):
+        coeffs = []
+        x = re.sub("\*\*\d", "", self.poly)
+        y = re.sub("[a-z]||[A-Z]||\'||\=||\,||\(||\)|| ", "", x)
+        z = re.split("\*", y)
+        print(z)
+        ctr = 0
+        coeffs.append(1)
+        for coef in z:
+            if coef != "":
+                coeffs.append(int(coef))
+            else:
+                ctr += 1
+        print(coeffs)
+        eigval = np.roots(coeffs)
+        print(eigval)
+        eig_list = eigval.tolist()
+        for i in range(ctr):
+            eig_list.append(0)
+        self.eigvalues = np.asarray(eig_list)
+        self.eigvalues.sort()
+        return self.eigvalues
+     
+    def D(self):
+        self.D_ = np.zeros((len(self.adjAA), len(self.adjAA)))
+        for i in range(len(self.eigvalues)):
+            for j in range(len(self.eigvalues)):
+                if i == j:
+                    self.D_[i][j] = sqrt(self.eigvalues[i])
+        return self.D_
+
+
+
+
+    def C(self):
+        matrices_I_ref = []
+        eigv_not_orth_str = []
+        self.eigvec = []
+        for i in range(len(self.eigvalues)):
+            self.eigvalues[i] = self.eigvalues[i] * 10000000 - self.eigvalues[i]*9999999
+            adjAA_minus_eigval = self.adjAA - self.eigvalues[i] * np.identity(len(self.adjAA))
+            adjAA_minus_eigval_T = np.transpose(adjAA_minus_eigval)
+            adjAA_minus_eigval_T_I = np.concatenate((adjAA_minus_eigval_T, np.identity(len(adjAA_minus_eigval_T))), axis = 1)
+            adjAA_minus_eigval_T_I_ref = common.ref(adjAA_minus_eigval_T_I)
+            adjAA_minus_eigval_Tref_Iref = np.split(adjAA_minus_eigval_T_I_ref, 2, axis = 1)
+            matrices_I_ref.append(adjAA_minus_eigval_Tref_Iref[1])
+        
+        for matrix in matrices_I_ref:
+            v = str(matrix[(len(matrix) - 1)])
+            if (v not in eigv_not_orth_str):
+                eigv_not_orth_str.append(v)
+            else:
+                j = 2
+                while v in eigv_not_orth_str:
+                    v = str(matrix[(len(matrix) - j)])
+                    j += 1
+                eigv_not_orth_str.append(v)
+        for string in eigv_not_orth_str:
+            s = re.split('\[|\]| ', string)
+            s1 = []
+            for i in range(len(s)):
+                if s[i] != "":
+                    s[i] = float(s[i])
+                    s1.append(s[i])
+            self.eigvec.append(s1)
+        print(self.eigvec)
+        self.eigvec = common.orthogonalization(self.eigvec)
+        self.eigvec = np.array(self.eigvec)
+        return self.eigvec
+
+
+
+    def B(self):
+        self.B_ = []
+        eigvec_t = self.eigvec
+        for i in range(len(eigvec_t)):
+            print('vec', eigvec_t[i], 'val', self.eigvalues[i])
+            if self.eigvalues[i] != 0:
+                b = np.dot(self.A, eigvec_t[i]) / sqrt(self.eigvalues[i])
+            else:
+                b = np.zeros(len(self.A))
+            self.B_.append(b)
+        self.B_ = np.array(self.B_)
+        self.B_ = np.transpose(self.B_)
+        return self.B_
+
+    def output(self):
+        print('B', self.B_)
+        print()
+        print('D', self.D_)
+        print()
+        print('C', self.eigvec)
+        print()
+        print('mult', np.dot(np.dot(self.B_, self.D_), self.eigvec))
+
+
+common = Common()
+'''svd = SVD()
+svd.svd([[1,2,1],
+        [4,1,3],
+        [3,5,2]])
+print(svd.adj())
+print(svd.find_char_pol())
+print(svd.coefficients())
+svd.D()
+svd.C()
+svd.B()
+svd.output()
+'''
